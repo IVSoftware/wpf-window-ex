1325119747b272ccb8a7997e8d8d1ce37e4b5d67
IVSoftware <tgregor@ivsoftware.com>
2020-05-28 17:08:55 -0600

    Added overrides

--- wpf-window-ex/WindowEx.cs
+++ wpf-window-ex/WindowEx.cs
@@ -1,7 +1,24 @@
 ﻿
+using System;
+
 namespace IVSoftware
 {
     public partial class Window : System.Windows.Window
     {
+        public Window()
+        {
+            Loaded += (object sender, System.Windows.RoutedEventArgs e)=> { };
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+        }
+        protected virtual void OnLoaded(System.Windows.RoutedEventArgs e) {  }
+        public event EventHandler HandleCreated;
+        protected virtual void OnHandleCreated (System.Windows.RoutedEventArgs e) 
+        {
+            HandleCreated?.Invoke(this, e);
+        }
     }
 }
